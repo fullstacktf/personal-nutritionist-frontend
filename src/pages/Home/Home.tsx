5fdad09b768569d90ebc5c839828681e4f117bf2
Elena Sacramento <elenasacramento80@gmail.com>
2021-12-06 14:50:11 +0000

    adding mui library to home component

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,11 +1,13 @@
 import './Home.css';
+import { Button } from '@mui/material';
+
 export default function Home() {
   return (
     <div>
       <div className="HomeImg">
         <h1>La web del cuidado personal</h1>
         <h2>Cómoda, accesible y la mejor manera de cuidarte a ti mismo.</h2>
-        <button>Inicia sesión</button>
+        <Button variant="contained">Inicia sesión</Button>
       </div>
     </div>
   );
