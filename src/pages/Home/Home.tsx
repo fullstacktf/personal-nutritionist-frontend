174ccc6b9662c2fb5233212f8b1bf26242d34ffb
Elena Sacramento <elenasacramento80@gmail.com>
2021-12-06 14:34:35 +0000

    adding home component with its test and css

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,12 @@
+import './Home.css';
+export default function Home() {
+  return (
+    <div>
+      <div className="HomeImg">
+        <h1>La web del cuidado personal</h1>
+        <h2>Cómoda, accesible y la mejor manera de cuidarte a ti mismo.</h2>
+        <button>Inicia sesión</button>
+      </div>
+    </div>
+  );
+}
