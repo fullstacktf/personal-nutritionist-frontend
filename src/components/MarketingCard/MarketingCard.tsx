44ad4236388871cbae172d90a7550228e4056a29
Elena Sacramento <elenasacramento80@gmail.com>
2021-12-10 20:01:17 +0000

    adding MarketingCard component

---
+++ src/components/MarketingCard/MarketingCard.tsx
@@ -0,0 +1,51 @@
+import { Card, CardContent, CardMedia, Typography, Grid } from "@mui/material";
+import { FC } from "react";
+import {styled} from "@mui/material/styles";
+
+interface Props {
+  cardImage: string;
+  name: string;
+  info: string;
+  altInfo: string;
+}
+const Container = styled(Card)(() => ({
+  height: 200,
+  display: "flex",
+}));
+
+const ContainerInfo = styled(CardContent)(() => ({
+  display: "flex",
+  flexDirection: "column",
+  flexGrow: 1,
+  justifyContent: "center",
+}));
+
+const Text = styled(Typography)(({theme}) => ({
+  [theme.breakpoints.down("md")]: {
+    fontSize: 20,
+    fontWeight: "bold"
+  }
+}));
+
+export const MarketingCard: FC<Props> = ({ name, cardImage, info, altInfo }) => {
+  return (
+    <Grid item xs={11} md={11} lg={7}>
+      <Container>
+        <CardMedia
+          component="img"
+          sx={{ width:{xs: 150, md: 300, lg: 400} }}
+          image={cardImage}
+          alt={altInfo}
+        />
+        <ContainerInfo>
+          <Text align="left" variant="h4">
+            {name}
+          </Text>
+          <Typography align="left" variant="subtitle1">
+            {info}
+          </Typography>
+        </ContainerInfo>
+      </Container>
+    </Grid>
+  );
+};
--- src/pages/Home/Home.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import Home from './Home';
-
-test('Home has catchword', () => {
-  render(<Home />);
-  const catchWord = screen.getByText(/La web del cuidado personal/i);
-  expect(catchWord).toBeInTheDocument();
-});
