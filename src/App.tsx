e92bf3e7b37c3614a0ebca682bea36b9f63a445d
Elena Sacramento <elenasacramento80@gmail.com>
2021-12-10 20:13:53 +0000

    fixing eslint problems in Home

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import './App.css';
-import Home from './pages/Home/Home';
+import React from "react";
+import "./App.css";
+import Home from "./pages/Home/Home";
 
 function App() {
   return (
