860d55f4ec102aa998c091bff3f79b9197b2131c
Adán de la Rosa Lugo <alu0100909012@ull.edu.es>
2021-12-15 23:21:26 +0000

    adding submit data with axios

--- src/components/LogIn/LogIn.tsx
+++ src/components/LogIn/LogIn.tsx
@@ -2,10 +2,11 @@ import { useState, ChangeEvent } from "react";
 import { Button, FormControl } from "@mui/material";
 import { InputForm } from "../InputForm/InputForm";
 import { FC } from "react";
+import axios from "axios";
 
 export const LogIn: FC = () => {
   const [data, setData] = useState({
-    name: "",
+    email: "",
     password: ""
   });
 
@@ -16,16 +17,21 @@ export const LogIn: FC = () => {
     });
   };
 
-  const submitData = () => {
-    
+  const submitData = (event: any ) => {
+    event.preventDefault();
+
+    axios.post("http://localhost:5000/auth/login", data)
+    .then(res => {
+      console.log(res.data);
+    });
   };
 
   return (
     <FormControl>
       <h2>Bienvenido a Nutriguide</h2>
       <p>¿Eres nuevo?<a href="a">Crear una cuenta</a></p>
-      <InputForm onChange={handleDataChange} name="name" placeholder="Usuario" type="username" validation={true} />
-      <InputForm onChange={handleDataChange} name="password" placeholder="Contraseña" type="password" validation={true} />
+      <InputForm onChange={handleDataChange} name="email" placeholder="Escribe tu email" type="email" validation={true} />
+      <InputForm onChange={handleDataChange} name="password" placeholder="Escribe tu contraseña" type="password" validation={true} />
       <Button onClick={submitData} variant="contained" >Iniciar Sesión</Button>
     </FormControl>
   );
