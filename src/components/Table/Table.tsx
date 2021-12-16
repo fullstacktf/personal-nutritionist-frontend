5c5c44fa6eaf7dd937a14b74de28b9fceb15d047
Míriam Núñez García <mirinu97@gmail.com>
2021-12-16 22:30:05 +0000

    Update src/components/Table/Table.tsx
    Co-authored-by: Elena Sacramento <43437557+elenamsd@users.noreply.github.com>

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -35,7 +35,6 @@ const StyledTableRow = styled(TableRow)(({ theme }) => ({
   "&:nth-of-type(odd)": {
     backgroundColor: theme.palette.action.hover,
   },
-  // hide last border
   "&:last-child td, &:last-child th": {
     border: 0,
   },
