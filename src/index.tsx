211edffac5c23f3cce3c8513fe00fa9747ae2e7f
Elena Sacramento <43437557+elenamsd@users.noreply.github.com>
2021-12-18 17:56:32 +0000

    Update src/index.tsx
    Co-authored-by: Míriam Núñez García <mirinu97@gmail.com>

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,6 @@ import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import { store } from "./app/store";
 
-//   document.getElementById("root")
 // );
 
 ReactDOM.render(
