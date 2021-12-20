667fdbedfe211f83b48a6e6d17f156c5b6b8ebe7
Adán de la Rosa Lugo <alu0100909012@ull.edu.es>
2021-12-20 11:10:28 +0000

    Update src/App.tsx
    Co-authored-by: Míriam Núñez García <mirinu97@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ import { LogInPage } from "./pages/LogInPage/LogInPage";
 import { PersonalPage } from "./pages/PersonalPage/PersonalPage";
 import { HealthPage } from "./pages/HealthPage/HealthPage";
 import { VerificationPage } from "./pages/VerificationPage/VerificationPage";
+
 const theme = createTheme ({
   palette: {
     primary: {
