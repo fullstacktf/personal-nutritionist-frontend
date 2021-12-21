2f246aa6d98506d06cf8af5e5a18ee81991659e9
Míriam Núñez García <mirinu97@gmail.com>
2021-12-21 20:32:30 +0000

    finishing home page

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react";
+import { CSSProperties, FC } from "react";
 
 import { Grid, Avatar, Typography, Button } from "@mui/material";
 import { styled } from "@mui/material/styles";
@@ -7,6 +7,17 @@ import { Phone, Mail } from "@mui/icons-material";
 interface Props {
   userInfo: any;
 }
+
+const ContainerStyle: CSSProperties = {
+  display: "flex",
+  justifyContent: "center",
+  flexDirection: "column",
+  width: "55%",
+  alignItems: "center",
+  backgroundColor: "white",
+  padding: "10px 10px 10px 10px",
+  borderRadius: "5px",
+};
 
 const GridContainer = styled(Grid) (() => ({
   display: "flex",
@@ -19,88 +30,41 @@ const GridContainer = styled(Grid) (() => ({
   width: "70%"
 }));
 
-const GridProfileContainer = styled(Grid) (() => ({
-  backgroundColor: "white",
-  width: "300px",
-  height: "70%",
-  alignItems: "center",
-  justifyContent: "center",
-  borderRadius: "5px",
-}));
-
 export const Profile: FC<Props> = ({ userInfo }) => {
   return (
-    // <div>
-    //   <div>
-    //     <Avatar variant="circular"  sx={{ width: 120, height: 120, marginBottom: 3 }} src={userInfo.photo}>{userInfo.name.charAt(0).toUpperCase()}</Avatar>
-    //     <Typography variant="subtitle1"><b>{userInfo.name}</b></Typography>
-    //     <Typography variant="subtitle1">{userInfo.role}</Typography>
-    //     <hr style={{ width: "50%" }}/>
-    //   </div>
-
-      // <div>
-      //   <Typography variant="subtitle1"><b>Descripción</b></Typography>
-      //   <Typography align="center">{ userInfo.description !== "" ? userInfo.description : "-" }</Typography>
-      //   <hr style={{ width: "50%"}}/>
-      // </div>
-
-      // <div>
-      //   {
-      //     userInfo.role === "Nutricionista" ?
-      //       <GridContainer item xs={12} lg={12} sx={{ height: "10%" }}>
-      //         <Typography variant="subtitle1"><b>Especialidades</b></Typography>
-      //         <Typography variant="subtitle1"> {userInfo.specialties} </Typography>
-      //         <hr style={{ width: "50%" }}/>
-      //       </GridContainer>
-      //     : 
-      //       <GridContainer item xs={12} lg={12} sx={{ height: "10%", marginTop: "1.5em" }}>
-      //         <Typography variant="subtitle1"><b>Datos médicos</b></Typography>
-      //         <Typography variant="subtitle1"><b>Altura</b>: { userInfo.height !== "" ? userInfo.height : "-" } </Typography>
-      //         <Typography variant="subtitle1"><b>Peso</b>: { userInfo.weight !== "" ? userInfo.weight : "-" } </Typography>
-      //         <hr style={{ width: "50%" }}/>
-      //       </GridContainer>
-      //   }
-      // </div>
-
-      // <div>
-      //   <Typography variant="subtitle1"><b>Contáctame</b></Typography>
-      //   <Button variant="contained" startIcon={<Phone />} sx={{ marginBottom: 2 }}>
-      //     { userInfo.phone === 0 ? "-" : userInfo.phone }
-      //   </Button>
-      //   <Button variant="contained" startIcon={<Mail />}>
-      //     {userInfo.email}
-      //   </Button>
-      // </div>
-    // </div>
-
-    <GridProfileContainer container item>
-      <GridContainer item lg={12} sx={{ height: "40%" }}>
-        <Avatar variant="circular"  sx={{ width: 120, height: 120, marginBottom: 3 }} src={userInfo.photo}>{userInfo.name.charAt(0).toUpperCase()}</Avatar>
+    <div style={ContainerStyle}>
+      <div style={ContainerStyle}>
+        <Avatar variant="circular" sx={{ width: 120, height: 120, marginBottom: 3 }} src={userInfo.photo}>{userInfo.name.charAt(0).toUpperCase()}</Avatar>
         <Typography variant="subtitle1"><b>{userInfo.name}</b></Typography>
         <Typography variant="subtitle1">{userInfo.role}</Typography>
         <hr style={{ width: "50%" }}/>
-      </GridContainer>
-      <GridContainer item xs={12} lg={12} sx={{ height: "20%", margin: "-1.5em 1em 0 1em" }}>
+      </div>
+
+      <div style={ContainerStyle}>
         <Typography variant="subtitle1"><b>Descripción</b></Typography>
         <Typography align="center">{ userInfo.description !== "" ? userInfo.description : "-" }</Typography>
         <hr style={{ width: "50%"}}/>
-      </GridContainer>
-      {
-        userInfo.role === "Nutricionista" ?
-          <GridContainer item xs={12} lg={12} sx={{ height: "10%" }}>
-            <Typography variant="subtitle1"><b>Especialidades</b></Typography>
-            <Typography variant="subtitle1"> {userInfo.specialties} </Typography>
-            <hr style={{ width: "50%" }}/>
-          </GridContainer>
-        : 
-          <GridContainer item xs={12} lg={12} sx={{ height: "10%", marginTop: "1.5em" }}>
-            <Typography variant="subtitle1"><b>Datos médicos</b></Typography>
-            <Typography variant="subtitle1"><b>Altura</b>: { userInfo.height !== "" ? userInfo.height : "-" } </Typography>
-            <Typography variant="subtitle1"><b>Peso</b>: { userInfo.weight !== "" ? userInfo.weight : "-" } </Typography>
-            <hr style={{ width: "50%" }}/>
-          </GridContainer>
-      }
-      <GridContainer item xs={10} lg={10} sx={{ height: "20%", flexDirection: "column" }}>
+      </div>
+
+      <div style={ContainerStyle}>
+        {
+          userInfo.role === "Nutricionista" ?
+            <GridContainer item xs={12} lg={12} sx={{ height: "10%" }}>
+              <Typography variant="subtitle1"><b>Especialidades</b></Typography>
+              <Typography variant="subtitle1"> {userInfo.specialties} </Typography>
+              <hr style={{ width: "50%" }}/>
+            </GridContainer>
+          : 
+            <GridContainer item xs={12} lg={12} sx={{ height: "10%", marginTop: "1.5em" }}>
+              <Typography variant="subtitle1"><b>Datos médicos</b></Typography>
+              <Typography variant="subtitle1"><b>Altura</b>: { userInfo.height !== "" ? userInfo.height : "-" } </Typography>
+              <Typography variant="subtitle1"><b>Peso</b>: { userInfo.weight !== "" ? userInfo.weight : "-" } </Typography>
+              <hr style={{ width: "50%" }}/>
+            </GridContainer>
+        }
+      </div>
+
+      <div style={ContainerStyle}>
         <Typography variant="subtitle1"><b>Contáctame</b></Typography>
         <Button variant="contained" startIcon={<Phone />} sx={{ marginBottom: 2 }}>
           { userInfo.phone === 0 ? "-" : userInfo.phone }
@@ -108,7 +72,7 @@ export const Profile: FC<Props> = ({ userInfo }) => {
         <Button variant="contained" startIcon={<Mail />}>
           {userInfo.email}
         </Button>
-      </GridContainer>
-    </GridProfileContainer>
+      </div>
+    </div>
   );
 };
